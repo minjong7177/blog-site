d5692b516941a5843d3fa327c636307e29de7ca3
minjong7177 <minjong7177@gmail.com>
2026-04-20 19:16:51 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__, template_folder="../templates")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Vercel용 핸들러
+def handler(request, context):
+    return app(request.environ, lambda status, headers: None)
