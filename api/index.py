f21761197370c9d5e32cd87ea94a607c1cdd6345
minjong7177 <minjong7177@gmail.com>
2026-04-20 19:59:01 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# Vercel용 핸들러
-def handler(request, context):
-    return app(request.environ, lambda status, headers: None)
