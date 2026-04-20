83a80b1cf5e243931dd16d16b13b1030b2e7b969
minjong7177 <minjong7177@gmail.com>
2026-04-20 19:26:20 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,6 +6,5 @@ app = Flask(__name__, template_folder="../templates")
 def home():
     return render_template("index.html")
 
-# Vercel용 핸들러
-def handler(request, context):
-    return app(request.environ, lambda status, headers: None)
+# ✅ Vercel에서 필요
+app.debug = False
